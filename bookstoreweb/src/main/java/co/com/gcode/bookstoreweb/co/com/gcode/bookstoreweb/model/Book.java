ae785e7296e5a7657453596ead060681598a733b
Giovany Villegas <givilleg@bancolombia.com.co>
2020-01-19 12:48:29 -0500

    Se ajusta el modelo para setearle const sin arg

--- bookstoreweb/src/main/java/co/com/gcode/bookstoreweb/co/com/gcode/bookstoreweb/model/Book.java
+++ bookstoreweb/src/main/java/co/com/gcode/bookstoreweb/co/com/gcode/bookstoreweb/model/Book.java
@@ -1,13 +1,17 @@
 package co.com.gcode.bookstoreweb.co.com.gcode.bookstoreweb.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Document
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Book {
 
